a1c1f814e2c81b672490bd145bc3d9c2f51988d7
Tosuoii <ibrahimahdev@gmail.com>
2026-05-02 22:43:21 +0100

    updating backend

---
+++ src/backend.py
@@ -0,0 +1,22 @@
+import math
+
+Properties = {
+    "Decks": 1,
+    "Players": 1,
+    "Cards": 52,
+    "Suits": 4,
+}
+
+CardTypes = [1,2,3,4,5,6,7,8,9,"Q","K","A"]
+
+
+
+def ReturnFirsthand():
+    print("Sending First Hand")
+
+
+def Hit():
+    print("Sending Hit")
+
+
+
