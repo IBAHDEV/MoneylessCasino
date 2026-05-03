e80196abe2fec3b119c555f0028193884e55b47d
TechnicalP576 <kushp2005@outlook.com>
2026-05-03 21:08:34 +0100

    Added App root, and started work on main menu

--- src/UI.py
+++ src/UI.py
@@ -1,2 +1,34 @@
-import customtkinter
+import customtkinter as ctk
+import backend
 
+#-------MAIN MENU-------
+#appearance
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+#root window
+app = ctk.CTk()
+app.title("MoneyLessCasino")
+app.geometry("600x400")
+
+#Menu frame
+menu_frame = ctk.CTkFrame(master=app)
+menu_frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+#title label 
+title_label = ctk.CTkLabel(master=menu_frame, text="MoneyLessCasino", font=("Roboto", 24))
+title_label.pack(pady=10)
+
+#buttons (Enter, Quit and Settings)
+enter_button = ctk.CTkButton(master=menu_frame, text="Enter", command=print("Entering Minigames........."))
+enter_button.pack(pady=10)
+
+settings_button = ctk.CTkButton(master=menu_frame, text="Settings", command=None)
+settings_button.pack(pady=10)
+
+exit_button = ctk.CTkButton(master=menu_frame, text="Exit", command=app.destroy)
+exit_button.pack(pady=10)
+
+
+
+app.mainloop()
